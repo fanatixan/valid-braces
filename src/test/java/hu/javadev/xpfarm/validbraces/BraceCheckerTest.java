f6e01fc063f050b98b736c511cb24f33534dec87
fejera <attila@javadev.hu>
2021-02-21 14:50:36 +0100

    completed test: it fails successfully :)

--- src/test/java/hu/javadev/xpfarm/validbraces/BraceCheckerTest.java
+++ src/test/java/hu/javadev/xpfarm/validbraces/BraceCheckerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class BraceCheckerTest {
 
     private BraceChecker checker;
@@ -21,6 +23,9 @@ class BraceCheckerTest {
 
         // when
         boolean valid = checker.isValid(input);
+
+        // then
+        assertThat(valid).isFalse();
     }
 
 }
