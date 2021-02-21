dc3da54e0c721d5e63f6110e35317a9cd21bb247
fejera <attila@javadev.hu>
2021-02-21 14:46:19 +0100

    adding test code until it fails

--- src/test/java/hu/javadev/xpfarm/validbraces/BraceCheckerTest.java
+++ src/test/java/hu/javadev/xpfarm/validbraces/BraceCheckerTest.java
@@ -1,7 +1,26 @@
 package hu.javadev.xpfarm.validbraces;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class BraceCheckerTest {
 
     private BraceChecker checker;
+
+    @BeforeEach
+    void setup() {
+        checker = new BraceChecker();
+    }
+
+    @Test
+    @DisplayName("single opening brace should be invalid")
+    void givenOpeningBraceOnly_isValid_shouldReturnFalse() {
+        // given
+        String input = "(";
+
+        // when
+        checker.isValid(input);
+    }
 
 }
