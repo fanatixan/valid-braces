eabf4094680c3db895648ae5fab9730675e1b24c
fejera <attila@javadev.hu>
2021-02-21 15:14:12 +0100

    refactor: simplified implementation

--- src/main/java/hu/javadev/xpfarm/validbraces/BraceChecker.java
+++ src/main/java/hu/javadev/xpfarm/validbraces/BraceChecker.java
@@ -1,25 +1,17 @@
 package hu.javadev.xpfarm.validbraces;
 
-import java.util.Map;
+import java.util.List;
 
 public class BraceChecker {
 
-    private static final Map<Character, Character> BRACE_PAIRS = Map.of(
-            '(', ')',
-            '[', ']',
-            '{', '}'
-    );
+    private static final List<String> VALID_BRACES = List.of("()", "[]", "{}");
 
     public boolean isValid(String input) {
         if (input.length() % 2 != 0) {
             return false;
         }
 
-        return isValidClosing(input.charAt(0), input.charAt(1));
-    }
-
-    private boolean isValidClosing(char opening, char closing) {
-        return BRACE_PAIRS.containsKey(opening) && BRACE_PAIRS.get(opening) == closing;
+        return VALID_BRACES.contains(input);
     }
 
 }
